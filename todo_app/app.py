ebd6ef4f56c89e3992eb02ea6c2f9eb28421c6b5
Chiara Dinan <chiara.dinan@softwire.com>
2024-01-23 22:50:55 +0000

    Exercise 1: Add functionality to add, complete, remove and display sorted items

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,11 +1,31 @@
-from flask import Flask
-
+from flask import Flask, render_template, request, redirect
+from todo_app.data.session_items import get_items, add_item, get_item, save_item, remove_item
 from todo_app.flask_config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config())
 
-
 @app.route('/')
 def index():
-    return 'Hello World!'
+    items = get_items()
+    to_dos = sorted(items, key=lambda x: x['status'], reverse=True)
+    return render_template("index.html", to_dos=to_dos)
+
+@app.route('/', methods=['POST'])
+def add_to_do():
+    new_item_title = request.form.get('new_to_do')
+    add_item(new_item_title)
+    return redirect('/')
+
+@app.route('/complete/<id>', methods=['POST'])
+def complete_to_do(id):
+    item = get_item(id)
+    item["status"] = "Completed"
+    save_item(item)
+    return redirect('/')
+
+@app.route('/delete/<id>', methods=['POST'])
+def delete_to_do(id):
+    item = get_item(id)
+    remove_item(item)
+    return redirect('/')
