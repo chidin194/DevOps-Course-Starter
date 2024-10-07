141433efbc0200d48962a0d42ef1825fc52d616f
Chiara Dinan <chiara.dinan@softwire.com>
2024-10-07 21:36:57 +0100

    Exercise 9: Revert test changes

--- todo_app/tests/test_integration.py
+++ todo_app/tests/test_integration.py
@@ -22,7 +22,7 @@ def test_index_page(monkeypatch, client):
 
     response = client.get('/')
     
-    assert response.status_code == 14035
+    assert response.status_code == 200
     assert 'Test card' in response.data.decode()
 
 
