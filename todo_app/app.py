1dd4fa52dffa5fa0239cd26ea022d3e5c5029985
Chiara Dinan <chiara.dinan@softwire.com>
2024-10-27 22:46:33 +0000

    Exercise 10: Replace Trello with MongoDB

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect
-from todo_app.data import trello_items
+from todo_app.data import mongo_items
 from todo_app.flask_config import Config
 from todo_app.view_models.ViewModel import ViewModel
 
@@ -10,7 +10,7 @@ def create_app():
 
     @app.route('/')
     def index():
-        items = trello_items.get_items()
+        items = mongo_items.get_items()
         view_model = ViewModel(items)
         
         return render_template("index.html", view_model=view_model)
@@ -19,19 +19,25 @@ def create_app():
     @app.route('/', methods=['POST'])
     def add_to_do():
         new_item_title = request.form.get('new_to_do')
-        trello_items.add_item(new_item_title)
+        mongo_items.add_item(new_item_title)
         return redirect('/')
 
 
     @app.route('/complete/<id>', methods=['POST'])
     def complete_to_do(id):
-        trello_items.complete_item(id)
+        mongo_items.complete_item(id)
+        return redirect('/')
+    
+
+    @app.route('/start/<id>', methods=['POST'])
+    def start_to_do(id):
+        mongo_items.start_item(id)
         return redirect('/')
 
 
     @app.route('/delete/<id>', methods=['POST'])
     def delete_to_do(id):
-        trello_items.delete_item(id)
+        mongo_items.delete_item(id)
         return redirect('/')
 
     return app
